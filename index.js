24c932186bdc11d3a40fb4de99f390d05afae484
smkisvarday <susannahkisvarday@hotmail.com>
2021-12-17 16:38:08 -0500

    I did it!

--- index.js
+++ index.js
@@ -1,58 +1,9 @@
-// var x = document.getElementById("btn1")[0];
-// if (x.id == "btn1") {
-//     x.style.fontSize = "300px";
-// }
+function display() {
+    var txt = document.getElementById("text_id").value;
+    document.getElementById("sk").innerHTML = txt;
+}
 
-//var buttonShort = document.createElement("button");
-//buttonShort.innerHTML = "Enter text then click this button";
-
-// var body = document.getElementsByTagName("body")[0];
-// body.appendChild(buttonShort);
-
-// buttonShort.addEventListener ("click", function() {
-//   var newWindow = window.open();
-//   newWindow.document.write("The generated Password is: '" + short() + "'");
-//   newWindow.focus()
-// });
-
-// var input;
-
-// function setup() {
-//     noCanvas ();
-//     input=createInput ();
-
-//     input.changed(newText);
-
-//     function newTtext() {
-//         console.log(input.value());
-//     }
-
-// }
-
-// function draw() {
-
-// }
-
-// const input = document.querySelector('input');
-// input.addEventListener('change', updateValue);
- 
-// function updateValue(e)
-// {
-// log.textContent = "This was just typed: " + e.target.value;
-// console.log("This was just typed: " +(e.target.value));
-// }
-
-// document.getElementById("text1").value;
-// console.log("The user just typed " +("text1".target.value));
-
-// <script type="text/javascript">
-//   function searchURL(){
-//     window.location = "http://www.myurl.com/search/" + (input text value);
-//   }
-// </script>
-
-
-let uname = document.querySelector("#uname");
+let text_id = document.querySelector("#text_id");
 function send() {
-  console.log(uname.value);
+  console.log(text_id.value);
 }
